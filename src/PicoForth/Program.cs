f93626f3b29b38f675a396fc3d0abd6e391567cc
enif77 <premysl.fara@gmail.com>
2023-02-02 21:01:05 +0100

    src dir, solution.

---
+++ src/PicoForth/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Copyright (C) Premysl Fara and Contributors */
+
+namespace PicoForth;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var ev = new Evaluator();
+            
+        ev.Eval("30 3 * 1 + 2 / . CR");
+        ev.Eval("30 3 !  3 @ . CR");
+        ev.Eval("( 30) 40 ( )3 !  3 @ . CR ( comment... )");
+
+        ev.Eval(".\" aaa\" 3 . CR");
+        ev.Eval("S\" bbb\" S. CR");
+    }
+}
+
+/*
+
+https://github.com/enif77/EFrt
+
+*/
