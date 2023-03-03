f79a228531e68f3863422aa9a0281ff01d9f9a06
enif77 <premysl.fara@gmail.com>
2023-03-03 13:52:55 +0100

    DUP word tests.

---
+++ src/PicoForth.Libs.Core.Tests/DupWordTests.cs
@@ -0,0 +1,60 @@
+/* Copyright (C) Premysl Fara and Contributors */
+
+namespace PicoForth.Libs.Core.Tests;
+
+using Xunit;
+
+using PicoForth.Extensions;
+using PicoForth.IO;
+using PicoForth.Libs.Core.Words;
+
+
+public class DupWordTests
+{
+    [Fact]
+    public void HasExpectedName()
+    {
+        Assert.Equal("DUP", new DupWord().Name);
+    }
+    
+    
+    [Fact]
+    public void IsNotImmediate()
+    {
+        Assert.False(new DupWord().IsImmediate);
+    }
+    
+    
+    [Fact]
+    public void GoesToTheNextWord()
+    {
+        var interpreter = new Interpreter(new NullOutputWriter());
+        
+        interpreter.StackPush(1);
+
+        Assert.Equal(1, new DupWord().Execute(interpreter));
+    }
+    
+    [Fact]
+    public void TopStackValueIsDuplicated()
+    {
+        var interpreter = new Interpreter(new NullOutputWriter());
+        
+        interpreter.StackPush(1);
+        
+        new DupWord().Execute(interpreter);
+        
+        Assert.Equal(1, interpreter.StackPop().Integer);
+        Assert.Equal(1, interpreter.StackPop().Integer);
+    }
+}
+
+/*
+
+https://forth-standard.org/standard/core/DUP
+
+Testing:
+
+T{ 1 DUP -> 1 1 }T
+
+ */
