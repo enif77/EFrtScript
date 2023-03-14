3e723335bd607e20839936bd4e6a7d167af9c698
Premysl Fara <premysl.fara@gmail.com>
2023-03-14 10:27:49 +0100

    Word READ-ALL-TEXT.

---
+++ src/EfrtScriptApp/Words/ReadAllTextWord.cs
@@ -0,0 +1,33 @@
+/* Copyright (C) Premysl Fara and Contributors */
+
+namespace EFrtScriptApp.Words;
+
+using EFrtScript;
+using EFrtScript.Extensions;
+
+
+internal class ReadAllTextWord : IWord
+{
+    public string Name => "READ-ALL-TEXT";
+    public bool IsImmediate => false;
+
+
+    public int Execute(IInterpreter interpreter)
+    {
+        interpreter.StackExpect(1);
+
+        interpreter.StackPush(
+            File.ReadAllText(
+                interpreter.StackPop().String));
+
+        return 1;
+    }
+}
+
+/*
+
+READ-ALL-TEXT
+
+Reads a file into a string on stack.
+
+*/
