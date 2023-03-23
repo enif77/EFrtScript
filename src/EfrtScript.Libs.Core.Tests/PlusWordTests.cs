533df13d087595051b7e388ef70d76c3acb48b24
Premysl Fara <premysl.fara@gmail.com>
2023-03-23 14:53:35 +0100

    The + word handles floating point numbers and string.

--- src/EfrtScript.Libs.Core.Tests/PlusWordTests.cs
+++ src/EfrtScript.Libs.Core.Tests/PlusWordTests.cs
@@ -47,7 +47,7 @@ public class PlusWordTests
     [InlineData(-1,  2,  1)]
     [InlineData(-1, -2, -3)]
     [InlineData(-1,  1,  0)]
-    public void CalculationResultsMatchExpectedMathResults(int a, int b, int expected)
+    public void CalculationResultsMatchExpectedIntegerMathResults(int a, int b, int expected)
     {
         var interpreter = new Interpreter(new NullOutputWriter());
         
@@ -58,23 +58,62 @@ public class PlusWordTests
         
         Assert.Equal(expected, interpreter.StackPop().Integer);
     }
+
+
+    [Theory]
+    [InlineData( 0.0,  5.0,  5.0)]
+    [InlineData( 5.0,  0.0,  5.0)]
+    [InlineData( 0.0, -5.0, -5.0)]
+    [InlineData(-5.0,  0.0, -5.0)]
+    [InlineData( 1.0,  2.0,  3.0)]
+    [InlineData( 1.0, -2.0, -1.0)]
+    [InlineData(-1.0,  2.0,  1.0)]
+    [InlineData(-1.0, -2.0, -3.0)]
+    [InlineData(-1.0,  1.0,  0.0)]
+    public void CalculationResultsMatchExpectedFloatingPointMathResults(double a, double b, double expected)
+    {
+        var interpreter = new Interpreter(new NullOutputWriter());
+        
+        interpreter.StackPush(a);
+        interpreter.StackPush(b);
+        
+        new PlusWord().Execute(interpreter);
+        
+        Assert.Equal(expected, interpreter.StackPop().Integer);
+    }
+
+
+    [Fact]
+    public void FirstIntIsConvertedToFloatingPointNumber()
+    {
+        var interpreter = new Interpreter(new NullOutputWriter());
+        
+        interpreter.StackPush(1);
+        interpreter.StackPush(1.0);
+        
+        new PlusWord().Execute(interpreter);
+        
+        var result = interpreter.StackPop();
+
+        Assert.True(result.IsRealValue());
+        Assert.Equal(2.0, result.Real);
+    }
+
+
+    [Fact]
+    public void SecondIntIsConvertedToFloatingPointNumber()
+    {
+        var interpreter = new Interpreter(new NullOutputWriter());
+        
+        interpreter.StackPush(1.0);
+        interpreter.StackPush(1);
+        
+        new PlusWord().Execute(interpreter);
+        
+        var result = interpreter.StackPop();
+
+        Assert.True(result.IsRealValue());
+        Assert.Equal(2.0, result.Real);
+    }
+
 }
-
-/*
-
-https://forth-standard.org/standard/core/Plus
-
-Testing:
-
-T{        0  5 + ->          5 }T
-T{        5  0 + ->          5 }T
-T{        0 -5 + ->         -5 }T
-T{       -5  0 + ->         -5 }T
-T{        1  2 + ->          3 }T
-T{        1 -2 + ->         -1 }T
-T{       -1  2 + ->          1 }T
-T{       -1 -2 + ->         -3 }T
-T{       -1  1 + ->          0 }T
-T{ MID-UINT  1 + -> MID-UINT+1 }T
-
- */
--- src/EfrtScript.Libs.Core/Words/PlusWord.cs
+++ src/EfrtScript.Libs.Core/Words/PlusWord.cs
@@ -15,8 +15,76 @@ internal class PlusWord : IWord
     {
         interpreter.StackExpect(2);
 
-        interpreter.StackPush(interpreter.StackPop().Integer + interpreter.StackPop().Integer);
+        var b = interpreter.StackPop();
+        var a = interpreter.StackPop();
+
+        if (a.IsStringValue() || b.IsStringValue())
+        {
+            interpreter.StackPush(a.String + b.String);
+        }
+        else if (a.IsRealValue() || b.IsRealValue())
+        {
+            interpreter.StackPush(a.Real + b.Real);
+        }
+        else
+        {
+            try   
+            {
+                checked
+                {
+                    interpreter.StackPush(a.Integer + b.Integer);
+                }
+            }
+            catch (OverflowException)
+            {
+                interpreter.StackPush(a.Real + b.Real);
+            }
+        }
 
         return 1;
     }
 }
+
+/*
+
+https://forth-standard.org/standard/core/Plus
+
++
+
+( n1 | f1 | s1  n2 | f2 | s2 -- n3 | f3 | s3 )
+Add n2 to n1, giving the sum n3.
+If the sum of n1 and n2 overflows, it converts both n1 and n2 to floating point values and returns a floating point f3.
+If either of the parameters is a floating point number, a floating point addition is performed.
+If either of the parameters is a string, the concatenetion is performed and s3 is returned.
+
+Testing:
+
+T{        0  5 + ->          5 }T
+T{        5  0 + ->          5 }T
+T{        0 -5 + ->         -5 }T
+T{       -5  0 + ->         -5 }T
+T{        1  2 + ->          3 }T
+T{        1 -2 + ->         -1 }T
+T{       -1  2 + ->          1 }T
+T{       -1 -2 + ->         -3 }T
+T{       -1  1 + ->          0 }T
+T{ MID-UINT  1 + -> MID-UINT+1 }T
+
+T{        0.0  5.0 + ->          5.0 }T
+T{        5.0  0.0 + ->          5.0 }T
+T{        0.0 -5.0 + ->         -5.0 }T
+T{       -5.0  0.0 + ->         -5.0 }T
+T{        1.0  2.0 + ->          3.0 }T
+T{        1.0 -2.0 + ->         -1.0 }T
+T{       -1.0  2.0 + ->          1.0 }T
+T{       -1.0 -2.0 + ->         -3.0 }T
+T{       -1.0  1.0 + ->          0.0 }T
+
+T{        1.0  1 + ->            2.0 }T
+T{          1  1.0 + ->          2.0 }T
+
+T{       "abc" "def" + ->   "abcdef" }T
+T{       "abc"   1 + ->       "abc1" }T
+T{           1 "abc" + ->     "1abc" }T
+
+ */
