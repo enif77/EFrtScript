5c5eb5aca821e705feb68aa8570e7c85e65b66db
Premysl Fara <premysl.fara@gmail.com>
2024-04-18 08:47:30 +0200

    The StringValue can be serialized to JSON.

--- src/EFrtScript/Values/StringValue.cs
+++ src/EFrtScript/Values/StringValue.cs
@@ -2,16 +2,20 @@
 
 namespace EFrtScript.Values;
 
+using System.Text.Json.Serialization;
 
 /// <summary>
 /// A string value.
 /// </summary>
 internal class StringValue : IValue
 {
+    [JsonIgnore]
     public bool Boolean => string.IsNullOrEmpty(String) == false;
 
+    [JsonIgnore]
     public int Integer => throw new NotSupportedException($"'{String}' cannot be converted to an integer number.");
 
+    [JsonIgnore]
     public double Float => throw new NotSupportedException($"'{String}' cannot be converted to a floating point number.");
 
     public string String { get; }
